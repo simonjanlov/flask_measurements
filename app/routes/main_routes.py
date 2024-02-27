15f8488c327ef8535c3d948cf28feddc51aa116d
simonjanlov <simon.janlov@gmail.com>
2024-02-27 21:11:11 +0100

    added code in main_routes to create a records model object from form submission

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request
+from app.models.records import RecordsModel
 from app.utils.graph_generator import generate_graph
+from collections import OrderedDict
 
 main_bp = Blueprint("main", __name__)
 
@@ -18,6 +20,37 @@ def weight_form():
         return render_template("pages/weight_form.html")
      
     if request.method == 'POST':
-        data = request.form
-        return data
+        # Create an ordered dict and convert values into floats
+        ordered_data = OrderedDict()
 
+        try:
+            # Move all items except "week_10" to the ordered_data
+            for key, value in request.form.items():
+                if key != 'week_10':
+                    ordered_data[key] = float(value)
+            # Add "week_10" at the end of ordered_data
+            if 'week_10' in request.form:
+                ordered_data['week_10'] = float(request.form['week_10'])
+        except ValueError as e:
+            return "Invalid input"
+
+        # Create an OrderedDict to preserve the order of insertion
+        # ordered_data = OrderedDict()
+        # # Move all items except "week_10" to the ordered_data
+        # for key, value in request.form.items():
+        #     if key != 'week_10':
+        #         ordered_data[key] = value
+        # # Add "week_10" at the end of ordered_data
+        # if 'week_10' in request.form:
+        #     ordered_data['week_10'] = request.form['week_10']
+
+        # # Print the ordered data for verification
+        # for key, value in ordered_data.items():
+        #     print(f"{key}: {value}")
+
+        # # Store in database
+        weight_data = RecordsModel(*list(ordered_data.values()))
+        print(weight_data)
+            
+        # Do something with the ordered data, such as processing the form data
+        return 'Form data processed successfully!'
